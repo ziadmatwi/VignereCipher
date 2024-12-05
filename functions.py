11347b1aada527cad08500e20a46bca9d0ad573e
ziadmatwi <ziadgabesz@gmail.com>
2024-12-05 18:55:41 +0100

    input funkció alap

---
+++ functions.py
@@ -0,0 +1,7 @@
+def inputfunction():
+    print("Üdv! Ez a program segít Vignere kódolt szöveget írni, olvasni, vagy feltörni.")
+    choice=int(input("Melyik funkciót választja? Szöveg írása(1) Szöveg beolvasása(2) Szöveg feltörése(3) Kilépés(0)"))
+    return choice
+
+def encoding():
+    
---
+++ main.py
@@ -0,0 +1,16 @@
+import lists
+import functions
+choice:int = 1
+
+
+while choice != 0:
+    choice = functions.inputfunction()
+    if choice == 0 :
+        print("asd")
+    elif choice == 1 :
+        print("asd")
+    elif choice == 2 :
+        print("asd")
+    else:
+        print("asd")
+
